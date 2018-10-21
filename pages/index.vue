e51b96cba41a37f70962fffeb04f8126409cd44b
yagisuke <yagisuke.0209@gmail.com>
2018-10-21 15:31:55 +0900

    use axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,13 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        Hello, world
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>qiitaの情報を取得しています</div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  async mounted() {
+    console.log(
+      JSON.stringify(await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:webgl'), true, '')
+    )
   }
 }
 </script>
