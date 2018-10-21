d56eff834c6f626414df03e8cc768a8db1fd8569
yagisuke <yagisuke.0209@gmail.com>
2018-10-21 16:47:50 +0900

    list layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,15 @@
   <div>
     <ul>
       <li v-for="item in items" :key="item.id">
-        <a :href="item.url" target="_blank">
-          <b>
-            <span>{{ item.title }}</span>
-            <small>{{ item.user.id }}</small>
-          </b>
-          <p>
+        <b>
+          {{ item.title }}
+          <nuxt-link :to="`/users/${item.user.id}`">{{ item.user.id }}</nuxt-link>
+        </b>
+        <div>
+          <a :href="item.url" target="_blank">
             {{ item.body.slice(0, 100) }}
-          </p>
-        </a>
+          </a>
+        </div>
       </li>
     </ul>
   </div>
@@ -20,7 +20,6 @@
 export default {
   async asyncData({ app }) {
     const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:webgl')
-
     return {
       items
     }
@@ -29,33 +28,4 @@ export default {
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
-
