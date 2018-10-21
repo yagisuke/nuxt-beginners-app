290bf3a88aaf8024ccff6d8f1361719e240fe8a6
yagisuke <yagisuke.0209@gmail.com>
2018-10-21 17:03:23 +0900

    create user page

--- pages/users/_userId.vue
+++ pages/users/_userId.vue
@@ -1,16 +1,38 @@
 <template>
-  <div>
-    <p>
-      /users/{{ userId }}.vue
-    </p>
-  </div>
+  <section>
+    <div>
+      <nuxt-link to="/">TOPへ戻る</nuxt-link>
+    </div>
+    <div>
+      <h3>{{ user.id }}</h3>
+      <img :src="user.profile_image_url" width="120" :alt="`${user.id}さんの画像`" />
+    </div>
+    <div>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <b>
+            {{ item.title }}
+            <nuxt-link :to="`/users/${item.user.id}`">{{ item.user.id }}</nuxt-link>
+          </b>
+          <div>
+            <a :href="item.url" target="_blank">
+              {{ item.body.slice(0, 100) }}
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  data() {
+  async asyncData({ route, app }) {
+    const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.userId}`)
+    const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.userId}`)
     return {
-      userId: this.$route.params.userId
+      user,
+      items
     }
   }
 }
