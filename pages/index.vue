7ab08e7d2c6fffb9bd497d307e4cd0cc563c67ab
yagisuke <yagisuke.0209@gmail.com>
2018-10-22 22:46:11 +0900

    top

--- pages/index.vue
+++ pages/index.vue
@@ -17,15 +17,13 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  async asyncData({ app }) {
-    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:webgl')
-    return {
-      items
-    }
-  }
+  async asyncData({ store }) {
+    if (store.getters['items'].length) return
+    await store.dispatch('fetchItems')
+  },
+  computed: mapGetters(['items'])
 }
 </script>
-
-<style>
-</style>
